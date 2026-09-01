3d8ade20c737838be61fcf67e50fdf139c44be84
sathishadobe <129248969+sathishadobe@users.noreply.github.com>
2026-09-01 15:35:45 +0530

    Update name-display.js

--- blocks/name-display/name-display.js
+++ blocks/name-display/name-display.js
@@ -1,1 +1,13 @@
 
+/**
+* loads and decorates the block
+* @param {Element} block The block element
+*/
+export default function decorate(block) {
+  [...block.children].forEach((row) => {
+    row.classList.add('test-block-row');
+    const [keyCell, valueCell] = row.children;
+    if (keyCell) keyCell.classList.add('test-block-key');
+    if (valueCell) valueCell.classList.add('test-block-value');
+  });
+}
